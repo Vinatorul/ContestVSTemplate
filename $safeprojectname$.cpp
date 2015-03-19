6b4e7dfcc42b51f81637d08bf75021f1e1e78a19
Vinatorul <vinetorul@yandex.ru>
2015-03-19 14:46:55 +0300

    Добавил проект

---
+++ $safeprojectname$.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstring>
+#ifdef HOME
+#include <cassert>
+#pragma warning (disable: 4996)
+#endif
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+#ifdef HOME
+  assert(freopen("input.txt", "r", stdin));
+  assert(freopen("output.txt", "w", stdout)); 
+#endif
+
+
+
+
+#ifdef HOME
+  cout << "Time: " << (int)(clock() * 1000. / CLOCKS_PER_SEC) << " ms\n";
+#endif
+  return 0;
+}
